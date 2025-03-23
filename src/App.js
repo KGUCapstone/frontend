7198b89a983b0ada1882ed8574568d92d50561bd
HanGwonhui <jana32496@gmail.com>
2025-03-23 21:07:29 +0900

    Docs: 기본설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Mainpage from "./pages/Mainpage";
+import Mypage from "./pages/Mypage"
+import Home from "./pages/Home";
+import Join from "./pages/Join";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/mainpage" element={<Mainpage />} />
+        <Route path="/mypage" element = {<Mypage />} />
+        
+        
+      </Routes>
+    </Router>
+
   );
-}
+};
 
 export default App;
