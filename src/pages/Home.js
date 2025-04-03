1b925fc145560cc29e4d51ae3d45be0d98124633
ldh0784 <ldhabc58@gmail.com>
2025-04-04 07:47:05 +0900

    ComparePage_추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ const Home = () => {
       <p>Welcome to the Home page!</p>
       <a href="/login">로그인</a> <br></br>
       <a href="/home">홈페이지</a>
+      <a href="/compareitem">아이템 비교하기</a>
+             {/*kkm이 바로 위에 수정함*/}
     </div>
   );
 };
