429fe034efbc74e39fa618ef9f0e2c90431eb1da
ldh0784 <ldhabc58@gmail.com>
2025-04-03 20:33:38 +0900

    Save changes before rebase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,12 @@
-
 const Home = () => {
-
-    return(
-        <div>
-            <h1>Home</h1>
-            <p>Welcome to the Home page!</p>
-
-            <a href="/login">로그인</a> <br></br>
-            <a href="/home">홈페이지</a>
-
-        </div>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Home page!</p>
+      <a href="/login">로그인</a> <br></br>
+      <a href="/home">홈페이지</a>
+    </div>
+  );
 };
 
 export default Home;
