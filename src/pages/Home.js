2cf852816bcb74bf96f554fe9175c8a2a118a6b0
ldh0784 <ldhabc58@gmail.com>
2025-04-04 12:38:11 +0900

    Feat: 검색 기능, 상품 비교 기능

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,12 @@ const Home = () => {
       <h1>Home</h1>
       <p>Welcome to the Home page!</p>
       <a href="/login">로그인</a> <br></br>
-      <a href="/home">홈페이지</a>
+      <a href="/home">홈페이지</a> <br></br>
       <a href="/compareitem">아이템 비교하기</a>
              {/*kkm이 바로 위에 수정함*/}
+      <br></br>
+      <a href="/search">상품 검색
+      </a>
     </div>
   );
 };
