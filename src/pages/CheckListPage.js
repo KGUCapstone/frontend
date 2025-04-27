00b55361676ac1502ff87d8fde5ea5d1e4b651ca
HanGwonhui <jana32496@gmail.com>
2025-04-27 22:50:22 +0900

    Feat: 체크리스트 빈 페이지 생성

---
+++ src/pages/CheckListPage.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import BottomNav from "../components/BottomNav";
+
+
+const CheckListPage = () => {
+   
+    return (
+        <div className="main-container">
+           <h1>체크리스트</h1>
+
+            <BottomNav />
+        </div>
+    );
+};
+
+export default CheckListPage;
