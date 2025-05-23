cb7b92e3c094ea539dbff96b6f7d54aa8c34df45
HanGwonhui <jana32496@gmail.com>
2025-05-23 11:37:34 +0900

    Feat: 로그아웃 전까지 로그인 유지 및 로그인 시 /, /login, /join 접근 불가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import titleImage from '../assets/title.svg';
 import logoImage from '../assets/logo.svg';
 import '../style/Home.css';
 
 const Home = () => {
-  const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
-  const handleStartClick = () => {
-    navigate('/login'); 
-  };
+    useEffect(() => {
+        const token = localStorage.getItem("Authorization");
+        if (token) {
+            navigate("/home");  // 토큰이 있으면 바로 홈으로 이동
+        }
+    }, [navigate]);
 
-  return (
-    <div className="home-container">
-      <div className="home-card">
-        <header>
-          <div className="home-title">
-            <img src={titleImage} alt="title" className="title-image" />
-            
+    const handleStartClick = () => {
+        navigate('/login');
+    };
 
-          </div>
-        </header>
+    return (
+        <div className="home-container">
+            <div className="home-card">
+                <header>
+                    <div className="home-title">
+                        <img src={titleImage} alt="title" className="title-image" />
+                    </div>
+                </header>
 
-        <img src={logoImage} alt="logo" className="logo-image" />
-        <p><span className="highlight-text">
-  가장 쉬운 <br />
-  비교 <br />
-  똑똑한 쇼핑 라이프</span>
-</p>
+                <img src={logoImage} alt="logo" className="logo-image" />
+                <p>
+                  <span className="highlight-text">
+                    가장 쉬운 <br />
+                    비교 <br />
+                    똑똑한 쇼핑 라이프
+                  </span>
+                </p>
 
-
-
-
-        <button onClick={handleStartClick} className="start-button">START</button>
-
-      </div>
-    </div>
-  );
+                <button onClick={handleStartClick} className="start-button">START</button>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
